9cdaacc59bc07839e348c2f598e53bcb1bad61e1
Russell <you@example.com>
2019-02-18 10:10:15 +0100

    make about and docs pages not redirect onload

---
+++ editor/src/firebase.js
@@ -0,0 +1,9 @@
+var config = {
+    apiKey: "AIzaSyBbtupCAPzGPHyAXQ_blEn5ooNVJFKwZqU",
+    authDomain: "splash-clouddata.firebaseapp.com",
+    databaseURL: "https://splash-clouddata.firebaseio.com",
+    projectId: "splash-clouddata",
+    storageBucket: "splash-clouddata.appspot.com",
+    messagingSenderId: "761544928567"
+};
+firebase.initializeApp(config);
